4888c64caa6788839e1b86cdf2e6efdab71b13be
qiji1987 <qij@163.com>
2019-08-13 09:08:40 +0800

    知识文件夹1

--- src/views/know/knowList.vue
+++ src/views/know/knowList.vue
@@ -26,9 +26,13 @@
                     <a-input />
                   </a-form-item>
                 </a-col>
-                <a-col :md="7" :sm="24">
-                  <a-form-item label="作者">
-                    <a-input />
+                <a-col :md="8" :sm="24">
+                  <a-form-item label="分类">
+                    <a-select placeholder="请选择" default-value="0">
+                      <a-select-option value="0">全部</a-select-option>
+                      <a-select-option value="1">分类1</a-select-option>
+                      <a-select-option value="2">分类2</a-select-option>
+                    </a-select>
                   </a-form-item>
                 </a-col>
                 <a-col :md="10" :sm="24">
@@ -40,23 +44,29 @@
                 </a-col>
               </a-row>
               <a-row>
-                <s-table
-                  ref="stable"
-                  size="default"
-                  :columns="columns"
-                  :data="loadData"
-                  :alert="false"
-                >
-                  <span slot="action" slot-scope="text, record">
-                    <template>
-                      <a @click="openEditModal(record,'2')" v-action:update>编辑</a>
-                      <a-divider type="vertical"/>
-                    </template>
-                    <template>
-                      <a @click="handleDel(record)" v-action:delete>删除</a>
-                    </template>
-                  </span>
-                </s-table>
+                <template>
+                  <a-list itemLayout="vertical" :pagination="pagination" :dataSource="listData">
+                    <a-list-item slot="renderItem" slot-scope="item, index" key="item.title">
+                      <template>
+                        <a-row>
+                          <a-col :span="18">
+                            <div style="word-wrap: break-word;word-break: break-all;overflow: hidden;">
+                              <a-list-item-meta :description="item.description">
+                                <a slot="title" :href="item.href">{{ item.title }},{{ index }}</a>
+                              </a-list-item-meta>
+                            </div>
+                          </a-col>
+                          <a-col :span="6"><a-button>详情</a-button></a-col>
+                        </a-row>
+                        <a-row>
+                          <a-col :span="24">
+                            <div style="word-wrap: break-word;word-break: break-all;overflow: hidden;">{{ item.content }}</div>
+                          </a-col>
+                        </a-row>
+                      </template>
+                    </a-list-item>
+                  </a-list>
+                </template>
               </a-row>
             </a-form>
           </div>
@@ -74,7 +84,18 @@ import STable from '@/components/Table/'
 import 'timers'
 import TabPane from 'ant-design-vue/es/vc-tabs/src/TabPane'
 
+// 测试数据,没有后端支持时前端写死
 const typeTest = [{ 'id': '01', 'parentId': 'root', 'children': [{ 'id': '0101', 'parentId': '01', 'children': [{ 'id': '010101', 'parentId': '0101', 'children': [], 'label': '第一个子分类', 'order': 1 }, { 'id': '010102', 'parentId': '0101', 'children': [], 'label': '第二个子分类', 'order': 2 }], 'label': '第一个分类', 'order': 1 }, { 'id': '0102', 'parentId': '01', 'children': [], 'label': '第二个分类', 'order': 2 }], 'label': '根节点分类', 'order': 1 }]
+const listData = []
+for (let i = 0; i < 10; i++) {
+  listData.push({
+    href: 'javascript:void(0)',
+    title: `个人知识 ${i}`,
+    avatar: '#',
+    description: '系统管理员   系统管理员   abcd    论文    2019-05-24 13:47:46   descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription',
+    content: '详情：contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent'
+  })
+}
 
 export default {
   name: 'KnowList',
@@ -86,11 +107,23 @@ export default {
     return {
       cardvisible: true,
       typeTree: typeTest,
-      TypeTreeSelects: []
+      TypeTreeSelects: [],
+      listData,
+      pagination: {
+        onChange: (page) => {
+          // 分页事件
+        },
+        pageSize: 3
+      }
     }
   },
   created () {
     this.getTypeTree()
+  },
+  actions: {
+    add () {
+      console.log(1)
+    }
   },
   methods: {
     genernateTree (value) {
@@ -110,7 +143,7 @@ export default {
     },
     handleClick (item, e) {
       this.queryParam = {
-        'orgCode': item[0]
+        'typeCode': item[0]
       }
       this.$refs.stable.refresh(true)
     }

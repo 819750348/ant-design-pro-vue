9a2ba9e8ad4e3355c0f8fba802404094078a18dd
819750348 <819750348@qq.com>
2019-08-16 13:09:43 +0800

    Merge branch 'master' of https://github.com/819750348/ant-design-pro-vue
    # Conflicts:
    #	src/views/knowledge/knowledgeTabs.vue

--- src/views/knowledge/creatApproval.vue
+++ src/views/knowledge/creatApproval.vue
@@ -1,62 +1,61 @@
 <template>
-  <div id="modals">
-    <a-modal
-      title="创建审批"
-      v-model="apply"
-      closable="false"
-      @ok="handleOk"
-      :okButtonProps="{ props: {disabled: true} }"
-      :cancelButtonProps="{ props: {disabled: true} }"
-    >
-      <a-card>
-        <div v-if="personalModal">
-          <a-row type="flex">
-            <a-col :span="4">
-              <a-tag color="#2f54eb" style="width: 80%; font-size: 16px">审批阶段</a-tag>
-            </a-col>
-            <a-col :span="5">
-              <a-tag color="#2f54eb" style="width: 90%; font-size: 16px">节点状态</a-tag>
-            </a-col>
-            <a-col :span="5">
-              <a-tag color="#2f54eb" style="width: 80%; font-size: 16px">审批人</a-tag>
-            </a-col>
-            <a-col :span="7">
-              <a-tag color="#2f54eb" style="width: 80%; font-size: 16px">审批意见</a-tag>
-            </a-col>
-            <a-col :span="3">
-              <a-tag color="#2f54eb" style="width: 40%; font-size: 16px">操作</a-tag>
-            </a-col>
-          </a-row>
-          <a-row type="flex" style="margin-top: 10px">
-            <a-col :span="4">
-              <span style="color: #333333">第一阶段</span>
-            </a-col>
-            <a-col :span="5">
-              <span style="color: #cf1322">刚刚创建</span>
-            </a-col>
-            <a-col :span="5">
-              <span style="color: #333333">待选择</span>
-            </a-col>
-            <a-col :span="7">
-              <span style="color: #333333">无</span>
-            </a-col>
-            <a-col :span="3">
-              <a-button type="primary" size="small" @click="showMajorModal">选择专业</a-button>
-            </a-col>
-          </a-row>
-        </div>
-        <div v-if="majorModal">
-          <major-tree @personnelModal="showPersonnelModal"></major-tree>
-        </div>
-        <div v-if="personnelModal">
-          <personnel-list></personnel-list>
-        </div>
-      </a-card>
-      <template slot="footer">
-        <a-button key="back" @click="handleCancel">关闭窗口</a-button>
-      </template>
-    </a-modal>
-  </div>
+  <a-modal
+    title="创建审批"
+    v-model="apply"
+    closable="false"
+    :bodyStyle="{ padding: '4px'}"
+    @ok="handleOk"
+    :okButtonProps="{ props: {disabled: true} }"
+    :cancelButtonProps="{ props: {disabled: true} }"
+  >
+    <a-card>
+      <div v-if="personalModal">
+        <a-row type="flex">
+          <a-col :span="4">
+            <a-tag color="#2f54eb" style="width: 80%; font-size: 16px">审批阶段</a-tag>
+          </a-col>
+          <a-col :span="5">
+            <a-tag color="#2f54eb" style="width: 90%; font-size: 16px">节点状态</a-tag>
+          </a-col>
+          <a-col :span="5">
+            <a-tag color="#2f54eb" style="width: 80%; font-size: 16px">审批人</a-tag>
+          </a-col>
+          <a-col :span="7">
+            <a-tag color="#2f54eb" style="width: 80%; font-size: 16px">审批意见</a-tag>
+          </a-col>
+          <a-col :span="3">
+            <a-tag color="#2f54eb" style="width: 40%; font-size: 16px">操作</a-tag>
+          </a-col>
+        </a-row>
+        <a-row type="flex" style="margin-top: 10px">
+          <a-col :span="4">
+            <span style="color: #333333">第一阶段</span>
+          </a-col>
+          <a-col :span="5">
+            <span style="color: #cf1322">刚刚创建</span>
+          </a-col>
+          <a-col :span="5">
+            <span style="color: #333333">待选择</span>
+          </a-col>
+          <a-col :span="7">
+            <span style="color: #333333">无</span>
+          </a-col>
+          <a-col :span="3">
+            <a-button type="primary" size="small" @click="showMajorModal">选择专业</a-button>
+          </a-col>
+        </a-row>
+      </div>
+      <div v-if="majorModal">
+        <major-tree @personnelModal="showPersonnelModal"></major-tree>
+      </div>
+      <div v-if="personnelModal">
+        <personnel-list></personnel-list>
+      </div>
+    </a-card>
+    <template slot="footer">
+      <a-button key="back" @click="handleCancel">关闭窗口</a-button>
+    </template>
+  </a-modal>
 </template>
 <script>
 import './model.less'
@@ -108,10 +107,4 @@ export default {
 }
 </script>
 <style scoped>
-  #modals > .ant-modal-body {
-    padding: 4px !important;
-    font-size: 14px;
-    line-height: 1.5;
-    word-wrap: break-word;
-  }
 </style>
